c91b2833b11241377feaa442f952c181117d3261
Francisco Palena <palenafrancisco@gmail.com>
2024-09-10 01:01:59 -0300

    Modificacion Login

--- Practica_3/src/components/login/Login.jsx
+++ Practica_3/src/components/login/Login.jsx
@@ -1,43 +1,44 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Login = () => {
     const [name, setName] = useState("");
-    const [letterDetected, setletterDetected] = useState(false)
+    const [letterDetected, setLetterDetected] = useState(false);
 
-    const handleChange = (e) =>{
-        
-        setName(e.target.value)
+    const handleChange = (e) => {
+        const newName = e.target.value;
+        setName(newName);
 
-        if(name.length >= 0 && (name.includes("o") || name.includes("O"))){
-            setletterDetected(true)
-            alert("Por favor, ¡Nombres de usuario sin la letra o!")
-        }else{
-            setletterDetected(false)
+        if (newName.includes("o") || newName.includes("O")) {
+            setLetterDetected(true);
+            alert("Por favor, ¡Nombres de usuario sin la letra o!");
+        } else {
+            setLetterDetected(false);
         }
-    }
+    };
 
-    const buttonAction = () =>{
-        if (name.length == 0 || name.includes("o") || name.includes("O")){
-            alert("Usuario inválido para registrarse")
-        }else{
-            alert("¡Usuario registrado correctamente!")
+    const buttonAction = () => {
+        if (name.length === 0 || name.includes("o") || name.includes("O")) {
+            alert("Usuario inválido para registrarse");
+        } else {
+            alert("¡Usuario registrado correctamente!");
         }
-    }
+    };
 
-    const estiloP = {color: letterDetected ? "red" : "white"}
+    const estiloP = { color: letterDetected ? "red" : "white" };
     const estiloButton = {
         background: letterDetected ? "red" : "#1a1a1a",
-        cursor: letterDetected ? "not-allowed" : "pointer"
-    }
+        cursor: letterDetected ? "not-allowed" : "pointer",
+    };
 
-    return(
+    return (
         <>
-            <input type="text" onChange={handleChange} placeholder="Name"/>
+            <input type="text" onChange={handleChange} placeholder="Name" />
             <p style={estiloP}>{name}</p>
-            <button style={estiloButton} onClick={buttonAction}>Registrar usuario</button>
+            <button style={estiloButton} onClick={buttonAction}>
+                Registrar usuario
+            </button>
         </>
     );
-}
+};
 
-
-export default Login
+export default Login;
